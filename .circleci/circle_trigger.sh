92c40fb246f828d1965e14c3c2ff203e0975b7ca
Andreas Bucksteeg <andreas@bucksteeg.de>
2020-08-11 23:12:38 +0200

    Update circle_trigger.sh

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -49,7 +49,7 @@ echo "################################################################"
 # <package_name1>=<path_segment1>,<path_segment2> 
 # <package_name2>=<path_segment3>,<path_segment4>
 #
-PACKAGES=$(<.circleci/packages.txt)
+PACKAGE_CONFIGS=$(<.circleci/packages.txt)
 
 echo
 echo "Searching for changes since commit [${LAST_COMPLETED_BUILD_SHA:0:7}] ..."
@@ -74,61 +74,72 @@ else
   echo "DEFAULT (MASTER/BRANCH) CHANGE DETECTION: Only taking changes of the last commits into account. Only workflows of packages which have been changed in this commit are triggered!"
 fi
 
-for PACKAGE in ${PACKAGES[@]}
+for PACKAGE_CONFIG in ${PACKAGE_CONFIGS[@]}
 do
 
-  echo " - Current Package: ${PACKAGE}"
+  echo " - Current Package Config: ${PACKAGE_CONFIG}"
   IFS='='
   read -ra ADDR <<< "${PACKAGE}"
-  echo " - Package:               ${ADDR[0]}"
-  echo " - Package Path Segments: ${ADDR[1]}"
+  PACKAGE=${ADDR[0]}
+  PACKAGE_PATH_SEGMENTS=${ADDR[1]}
+  echo " - Package:               ${PACKAGE}"
+  echo " - Package Path Segments: ${PACKAGE_PATH_SEGMENTS}"
   
   IFS=','
-  read -ra PATHSEGMENTS <<< "${ADDR[1]}"
+  read -ra PATHSEGMENTS <<< "${PACKAGE_PATH_SEGMENTS}"
   
-  for PATHSEGMENT in ${PATHSEGMENTS[@]}; do
-    echo "--> ${PATHSEGMENT}"
-    PACKAGE_PATH=${ROOT#.}/${PATHSEGMENT}
-
-    if [ -n "${CIRCLE_PULL_REQUEST}" ]; then
-      LATEST_COMMIT_SINCE_LAST_BUILD=$(git --no-pager log origin/${PARENT_BRANCH}..${CIRCLE_BRANCH} --name-only --oneline -- ${PACKAGE} | sed '/ /d' | sed '/\//!d' | sed 's/\/.*//' | sort | uniq)
-      echo "git --no-pager log origin/${PARENT_BRANCH}..${CIRCLE_BRANCH} --name-only --oneline -- ${PACKAGE} | sed '/ /d' | sed '/\//!d' | sed 's/\/.*//' | sort | uniq)"
-      if [ -n "${LATEST_COMMIT_SINCE_LAST_BUILD}" ]; then
-        LATEST_COMMIT_SINCE_LAST_BUILD="## PR ##"
-      fi
-    else
-      LATEST_COMMIT_SINCE_LAST_BUILD=$(git log -1 $LAST_COMPLETED_BUILD_SHA..$CIRCLE_SHA1 --format=format:%H --full-diff -- ${PACKAGE_PATH#/})
-    fi
-
-    if [[ -z "$LATEST_COMMIT_SINCE_LAST_BUILD" ]]; then
-      INCLUDED=0
-      for FAILED_BUILD in ${FAILED_WORKFLOWS[@]}; do
-        if [[ "$PACKAGE" == "$FAILED_BUILD" ]]; then
-          INCLUDED=1
-          PARAMETERS+=", \"$PACKAGE\":true"
-          COUNT=$((COUNT + 1))
-          echo -e "\e[36m  [+] ${PACKAGE} \e[21m (included because failed since last build)\e[0m"
-          break
-        fi
-      done
-
-      if [[ "$INCLUDED" == "0" ]]; then
-        echo -e "\e[90m  [-] $PACKAGE \e[0m"
-      fi
-    else
-      PARAMETERS+=", \"$PACKAGE\":true"
-      COUNT=$((COUNT + 1))
-      echo -e "\e[36m  [+] ${PACKAGE} \e[21m (changed in [${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}])\e[0m"
-    fi
-  done
-
-  if [[ $COUNT -eq 0 ]]; then
-    echo -e "\e[93mNo changes detected in packages. Skip triggering workflows.\e[0m"
-    exit 0
+  if [ -n "${CIRCLE_PULL_REQUEST}" ]; then
+    NOPR=""
+  else
+    NOPR="-1"
   fi
 
-done
+  CHANGED_PATH_SEGMENTS=$(git --no-pager log $NOPR origin/${PARENT_BRANCH}..${CIRCLE_BRANCH} --name-only --oneline | sed '/ /d' | sed '/\//!d' | sed 's/\/.*//' | sort | uniq)
+  echo "git --no-pager log $NOPR origin/${PARENT_BRANCH}..${CIRCLE_BRANCH} --name-only --oneline | sed '/ /d' | sed '/\//!d' | sed 's/\/.*//' | sort | uniq"
+  for CHANGED_PATH_SEGMENT in ${CHANGED_PATH_SEGMENTS[@]}; do
+    for PATH_SEGMENT in ${PACKAGE_PATH_SEGMENTS[@]}; do
+      echo "${PATH_SEGMENT} == ${CHANGED_PATH_SEGMENT}?"
+      if [ "${PATH_SEGMENT}" == "${CHANGED_PATH_SEGMENT}" ]; then
+        CHANGE_DETECTED="true"
+        break
+      else
+        CHANGE_DETECTED="false"
+      fi
+    done
+    if [ "${CHANGE_DETECTED}" == "true" ]; then
+      break
+    fi
+  done
+  echo " => ${PATH_SEGMENT} == ${CHANGED_PATH_SEGMENT}? ${CHANGE_DETECTED}"
   
+  if [ "${CHANGE_DETECTED}" == "false" ]; then
+    INCLUDED=0
+    for FAILED_BUILD in ${FAILED_WORKFLOWS[@]}; do
+      if [[ "$PACKAGE" == "$FAILED_BUILD" ]]; then
+        INCLUDED=1
+        PARAMETERS+=", \"$PACKAGE\":true"
+        COUNT=$((COUNT + 1))
+        echo -e "\e[36m  [+] ${PACKAGE} \e[21m (included because failed since last build)\e[0m"
+        break
+      fi
+    done
+
+    if [[ "$INCLUDED" == "0" ]]; then
+      echo -e "\e[90m  [-] $PACKAGE \e[0m"
+    fi
+  else
+    PARAMETERS+=", \"$PACKAGE\":true"
+    COUNT=$((COUNT + 1))
+    echo -e "\e[36m  [+] ${PACKAGE} \e[21m (changed in [${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}])\e[0m"
+    break
+  fi
+done
+
+if [[ $COUNT -eq 0 ]]; then
+  echo -e "\e[93mNo changes detected in packages. Skip triggering workflows.\e[0m"
+  exit 0
+fi
+
 echo "Changes detected in ${COUNT} package(s)."
 
 ############################################
